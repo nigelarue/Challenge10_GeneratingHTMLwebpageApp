cd266e51938c0088855e67b898b8c1d8f0239a29
nigelarue <larue.nigel91@gmail.com>
2023-01-18 18:31:07 -0600

    created requirements within index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
+const Intern = require('./lib/intern');
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
